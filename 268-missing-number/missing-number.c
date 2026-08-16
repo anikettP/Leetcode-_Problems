0acbd17e1d8015f0a890675eaa99c13355fff91b
Aniii <158577927+anikettP@users.noreply.github.com>
2026-08-16 14:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (81.30%) - LeetSync

---
+++ 268-missing-number/missing-number.c
@@ -0,0 +1,8 @@
+int missingNumber(int* nums, int numsSize) {
+    int n=numsSize;
+    int expected=n*(n+1)/2,actual=0;
+    for(int i=0;i<n;i++){
+        actual+=nums[i];
+    }
+    return expected-actual;
+}
