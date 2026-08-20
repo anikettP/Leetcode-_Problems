3881b8ca7c7d1785b63fbc6bfeb4a15d6907e42d
Aniii <158577927+anikettP@users.noreply.github.com>
2026-08-20 10:06:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (13.64%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.c
@@ -0,0 +1,21 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** stringMatching(char** words, int wordsSize, int* returnSize) {
+    char** result=malloc(wordsSize*sizeof(char*));
+    *returnSize=0;
+    for (int i=0;i<wordsSize;i++){
+        for (int j=0;j<wordsSize;j++) {
+            if (i == j){
+                continue;
+            }
+            if (strstr(words[j],words[i])!=NULL) {
+                result[*returnSize]=words[i];
+                (*returnSize)++;
+                break;
+            }
+        }
+    }
+
+    return result;
+}
