0707b9b2536b2a5bec4b11c87dd85c4ac1094cc2
Aniii <158577927+anikettP@users.noreply.github.com>
2026-08-21 09:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (51.16%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.c
@@ -0,0 +1,14 @@
+char* clearDigits(char* s) {
+    int n = strlen(s);
+    char* stack=(char*)malloc((n + 1) * sizeof(char));
+    int top=-1;
+    for (int i=0;i<n;i++){
+        if (isdigit(s[i])){
+            top--;
+        } else{
+            stack[++top]=s[i];
+        }
+    }
+    stack[top + 1]='\0';
+    return stack;
+}
