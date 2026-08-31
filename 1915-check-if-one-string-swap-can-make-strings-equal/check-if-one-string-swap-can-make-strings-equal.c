9d5365cebc91951644a391ffe3f811dd386a410d
Aniii <158577927+anikettP@users.noreply.github.com>
2026-08-31 14:20:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (97.96%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.c
@@ -0,0 +1,21 @@
+bool areAlmostEqual(char* s1, char* s2) {
+    int first=-1,second=-1,count=0;
+    for (int i=0;s1[i]!='\0'; i++){
+        if (s1[i]!=s2[i]){
+            count++;
+            if (count == 1){
+                first = i;
+            }
+            else if(count==2){
+                second = i;
+            }
+        }
+    }
+    if (count==0){
+        return true;
+    }
+    if(count!=2){
+        return false;
+    }
+    return s1[first]==s2[second] && s1[second]==s2[first];
+}
