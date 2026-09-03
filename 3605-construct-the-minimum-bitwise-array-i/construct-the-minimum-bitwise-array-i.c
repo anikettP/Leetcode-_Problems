1bb83a7f09531aa386b3fe1730ef7d4df08c425d
Aniii <158577927+anikettP@users.noreply.github.com>
2026-09-03 09:53:55 +0530

    Time: 8 ms (6.25%) | Memory: 12.3 MB (77.08%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.c
@@ -0,0 +1,17 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* minBitwiseArray(int* nums, int numsSize, int* returnSize) {
+    int* ans=(int*)malloc(numsSize*sizeof(int));
+    for (int i=0;i<numsSize;i++) {
+        ans[i]=-1;
+        for (int x=0;x<= nums[i]; x++) {
+            if ((x | (x + 1)) == nums[i]) {
+                ans[i] = x;
+                break;
+            }
+        }
+    }
+    *returnSize = numsSize;
+    return ans;
+}
