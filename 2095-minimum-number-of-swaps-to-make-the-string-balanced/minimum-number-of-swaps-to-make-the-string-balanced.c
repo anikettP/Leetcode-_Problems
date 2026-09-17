40a0d10327d17fad6991784c2b656675d90c98cd
Aniii <158577927+anikettP@users.noreply.github.com>
2026-09-17 10:02:17 +0530

    Time: 11 ms (66.67%) | Memory: 16.3 MB (8.33%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.c
@@ -0,0 +1,15 @@
+int minSwaps(char* s) {
+    int open=0,close=0;
+    for (int i=0;s[i];i++){
+        if (s[i]=='['){
+            open++;
+        }
+        else if(open>0){
+            open--;
+        }
+        else{
+            close++;
+        }
+    }
+    return (close+1)/2;
+}
